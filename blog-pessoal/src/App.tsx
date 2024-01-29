dd04a8ad5b5a9dc78233b1a6db8c9ca2a3a00165
Pedro Soares <pedrowesleyfs@gmail.com>
2024-01-29 14:14:06 -0300

    implementado a postagem e lógica

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
 import './App.css';
 
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './pages/login/Login';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import DeletarPostagem from './components/postagens/deletarPostagem/deletarPostagem';
+import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem';
+import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import { AuthProvider } from './contexts/AuthContext';
 import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/home/Home';
-import { AuthProvider } from './contexts/AuthContext';
+import Login from './pages/login/Login';
 
 
 function App() {
@@ -22,6 +27,14 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+              <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
             </Routes>
           </div>
           <Footer />
--- blog-pessoal/src/components/navbar/Navbar.tsx
+++ blog-pessoal/src/components/navbar/Navbar.tsx
@@ -1,24 +1,36 @@
+import { useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import React from 'react'
+import logo from "../../assets/BlogYourMind.svg"
+import { AuthContext } from '../../contexts/AuthContext'
+
 
 
 function Navbar() {
- 
+  let navigate = useNavigate()
+
+  const { usuario, handleLogout } = useContext(AuthContext)
+
+  function logout() {
+      handleLogout()
+      alert('Usuário deslogado com sucesso')
+      navigate('/login')
+  }
+
+  let navbarComponent
   
 
   return (
     <>
-     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+     <div className='w-full bg-[#000932] text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
+            <img src={logo} alt="" className='w-1/6' />
 
             <div className='flex gap-4'>
-          
-              <div className='hover:underline'>Postagens</div>
-              <div className='hover:underline'>Temas</div>
-              <div className='hover:underline'>Cadastrar tema</div>
-              <div className='hover:underline'>Perfil</div>
-              <div className='hover:underline'>Sair</div>
+            <div className='hover:underline self-center'>Postagens</div>
+            <Link to='/temas' className='hover:underline self-center'>Temas</Link>
+              <Link to='/cadastroTema' className='hover:underline self-center'>Cadastrar tema</Link>
+              <div className='hover:underline self-center'>Perfil</div>
+              <Link to='' onClick={logout} className='hover:underline self-center'>Sair</Link>
              
             </div>
           </div>
